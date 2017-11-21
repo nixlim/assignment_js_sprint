3ffbc5de8c3ae3daf64b0ed248bf0e37a4ba4b5b
Igor Ryabchuk <riabtchuk@gmail.com>
2017-11-21 23:17:41 +0300

    worked out array comparison function

--- scripts.js
+++ scripts.js
@@ -3,12 +3,30 @@
 var sprintFunctions = {
   largestEl: function(someArray) {
       //Method 1 - the Easy Way
-      return someArray.reduce((a, b) => Math.max(a, b));
+      // return someArray.reduce((a, b) => Math.max(a, b));
+      //Method 2 - the Loop
+      //First, we set the maximum value to the first element in the array.
+      //Then we loop through the array, comparing each element to our maximum and changing maximum as appropriate
+      var max = someArray[0];
+      for (i=1; i<someArray.length; i++){
+          if (someArray[i]>max){
+              max = someArray[i];
+          }
+      }
+      return max;
+
   },
 
   reversed: function(string){
       //Method 1 - the Easy Way
-      return string.split("").reverse().join("");
+      // return string.split("").reverse().join("");
+      //Method 2 - the Loop
+      //Strings are immutable, stupid! Build new string from old string, back to front.
+      var newStr = "";
+      for (i = string.length;i > -1; i--) {
+          newStr = newStr + string.charAt(i);
+          }
+      return newStr;
   },
 
   loudSnakeCase: function(string){
@@ -18,9 +36,22 @@ var sprintFunctions = {
       ).join("_");
   },
 
-  compareArrays: function(){
-    // your code here (replace the return)
-    return "Finish compareArrays first!"
+  compareArrays: function(array1, array2){
+      //set results placeholder
+      var resultsArray = [];
+      //check the length of arrays
+      if (array1.length == array2.length) {
+          //compare the array element by element and populate placeholder array
+          for (i = 0; i < array1.length; i++) {
+              if (array1[i] === array2[i]){
+                  resultsArray[i] = true;
+              } else {
+                  resultsArray[i] = false;
+              }
+          }
+          //return the correct value for the results array
+          return resultsArray.includes(false) ?  false : true;
+      }
   },
 
   fizzBuzz: function(){
